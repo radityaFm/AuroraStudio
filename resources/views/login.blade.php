79152c52384930e90a6b86c006d3091e5041fcd2
radityaFm <140126229+radityaFm@users.noreply.github.com>
2024-04-27 20:02:52 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,8 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         /* warna navbar */
+        @font-face{
+        font-family: "font";
+        src: url(font.ttf);
+    }
         .navbar-custom {
-          background-color: #17a2b8;
+            background: linear-gradient(70deg, rgb(135,206, 250) 20%, rgb(0,191,255));
         }
         tubuh login
         body {
@@ -25,9 +29,12 @@
     .login-container {
         background-color: #fff;
         border-radius: 5px;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        box-shadow: 0px px 6px rgba(0, 0, 0.1);
         padding: 20px;
         width: 350px;
+    }
+    .navbar-brand{
+        padding:-10px;
     }
     /* input */
     input[type="text"],
@@ -73,13 +80,17 @@
     .login{
         margin-bottom: 15px;
     }
+    .logo{
+      width: 12%;
+      height:fill;
+    }
 </style>
 </style>
 </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
         <div class="container-fluid">
-          <a class="navbar-brand"  href="#">Navbar</a>
+          <a class="navbar-brand"  href="#"><img src="logo.png"alt="" class="logo"></a>
         </div>
       </nav>
       <br>
@@ -91,12 +102,10 @@
                 <img src="" alt="">
             </div>
           </div>
-          <!-- <div class="col-6">
-            <div class="p-3"></div>
-          </div> -->
-        <div class="login-container col-6 p-3">
+          <!-- login / sign in -->
+        <div class="login-container col-6 p-3 shadow p-3 mb-5 bg-body-tertiary rounded">
             <h2 class="mb-4">Login</h2>
-            <form id="loginForm" onsubmit="return validateForm()">
+            <form id="loginForm" onsubmit="return validateForm()" class="form">
                 <p class="email">Email
                     <input type="text" id="usernameEmail" placeholder="Username or Email" class="mb-1">
                 </p>
@@ -107,8 +116,11 @@
                 <br>
                 <input type="submit" value="Login" class="login mb-3">
             </form>
+            <!-- <div class="forgot-password mt-2" style="text-decoration: none;">
+                <a href="lupa" onclick="forgotPassword()">forgotpassword</a>
+            </div> -->
             <div class="forgot-password mt-2" style="text-decoration: none;">
-                <a href="#" onclick="forgotPassword()">Forgot Password?</a>
+                <a href="registrasi" onclick="registrasi()"><b>Registrasi</b></a>
             </div>
         </div>
         
@@ -118,7 +130,7 @@
                 var password = document.getElementById('password').value.trim();
         
                 // Basic validation
-                if (usernameEmail === 'garena' || password === 'freefire') {
+                if (usernameEmail === '1' || password === '1') {
                     alert('Username/Email and password are required.');
                     return false;
                 }
@@ -131,9 +143,9 @@
                     // Send a reset password link or perform further action
                     alert('A password reset link has been sent to your email.');
                 }
+                
             }
         </script>
-        
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
